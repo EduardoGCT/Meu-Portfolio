e99f346f29bc7edf246b051b35218328a2a77de3
EduardoGCT <eduardoterragomes@gmail.com>
2026-05-07 17:05:30 -0300

    Implementado a seção 'sobre'

--- script.js
+++ script.js
@@ -15,4 +15,38 @@ document.querySelectorAll('nav a').forEach(link => {
     });
 });
 
+//terminal simulado
+const input = document.getElementById('terminal-input');
+const output = document.getElementById('terminal-output');
+
+input.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        const command = input.value.trim().toLowerCase();
+        let response = "";
+
+        if (command === 'clear') {
+            output.innerHTML = ""; // Limpa todo o conteúdo
+            input.value = "";
+            return; // Encerra a função aqui
+        } 
+        
+        if (command === 'skills') {
+            response = "Python, Django, React, Linux, SQL.";
+        } else if (command === 'status') {
+            response = "3º Semestre de CC | Buscando Estágio.";
+        } else if (command === 'help') {
+            response = "Comandos: skills, status, clear.";
+        } else if (command !== "") {
+            response = `Comando não encontrado: ${command}`;
+        }
+
+        if (command !== "") {
+            output.innerHTML += `<p><span class="prompt">$ ${command}</span><br>${response}</p>`;
+        }
+        
+        input.value = "";
+        output.scrollTop = output.scrollHeight;
+    }
+});
+
 
